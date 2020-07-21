497578c0d433837ca215cdfc4033b792a95924b0
Shubham Jha <shubham.jha2001m@gmail.com>
2020-07-21 14:03:47 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     );
   }
 }
