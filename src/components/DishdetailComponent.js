c533845f599353faf9f8f12993db0929d58f79ab
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-01 15:26:28 +0700

    Single Page Applications Part 2
    Learnt the use of parameters within the URL to pass information to another component.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 // Task 3
 function RenderComments({ comments }) {
@@ -20,21 +21,20 @@ function RenderComments({ comments }) {
                 </p>
             </li>
         )
-    })
+    });
     return (
-        <div className='col-12 col-md-5 m-1'>
+        <div>
             <h4> Comments </h4>
             <ul className='list-unstyled'>
                 {commts}
             </ul>
         </div>
-    )
+    );
 }
 
 function RenderDish ({ dish }) {
     if (dish != null) {
         return (
-            <div className='col-12 col-md-5 m-1'>
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
@@ -42,11 +42,10 @@ function RenderDish ({ dish }) {
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-            </div>
-        )
+        );
     }
     else {
-        return (<div></div>)
+        return (<div></div>);
     }
 }
 
@@ -55,16 +54,29 @@ const Dishdetail = (props) => {
     if (dish == null) {
         return (<div></div>)
     }
-    const dishItem = <RenderDish dish={props.dish} />
-    const commentItem = <RenderComments comments = {props.dish.comments}/>
+    
     return (
         <div className="container">
-            <div className='row'>
-                {dishItem}
-                {commentItem}
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 
