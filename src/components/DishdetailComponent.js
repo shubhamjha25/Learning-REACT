c173c448d7562e24cbd662965430fd09fa192fad
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-09 12:46:27 +0700

    Assignment 3
    Updated the Dishdetail Component. Included a form and did the validation using the react-redux-form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from "react";
+import { Card, CardImg, CardBody,CardText, Button, Modal, ModalHeader, ModalBody, Label, Row, Col, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-// Task 3
-function RenderComments({ comments }) {
+function RenderComments({comments}) {
     if (comments == null) {
         return (<div></div>)
-    }
+    }  
+
     const commts = comments.map(comment => {
-        return (
-            <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>-- {comment.author},
-                &nbsp;
-                {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
+        return(
+            <li key={comment.id} >
+                    <p>{comment.comment}</p>
+                    <p>--{comment.author},
+                    {new Intl.DateTimeFormat('en-US', { 
+                        year: 'numeric', 
+                        month: 'short', 
                         day: '2-digit'
-                    }).format(new Date(comment.date))}
-                </p>
+                    }).format(new Date(Date.parse(comment.date)))}
+                    </p>
             </li>
         )
     });
-    return (
-        <div>
-            <h4> Comments </h4>
-            <ul className='list-unstyled'>
-                {commts}
-            </ul>
-        </div>
+    return(
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {commts}
+                </ul>
+                <CommentForm>
+
+                </CommentForm>
+            </div>
     );
 }
 
-function RenderDish ({ dish }) {
+function RenderDish({dish}) {
     if (dish != null) {
-        return (
+        return(
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
@@ -42,19 +45,21 @@ function RenderDish ({ dish }) {
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-        );
+        )
     }
-    else {
-        return (<div></div>);
+    else{
+        return(
+            <div></div>
+        )
     }
 }
+
 
 const Dishdetail = (props) => {
     const dish = props.dish
     if (dish == null) {
         return (<div></div>)
     }
-    
     return (
         <div className="container">
             <div className="row">
@@ -65,19 +70,112 @@ const Dishdetail = (props) => {
                 <div className="col-12">
                     <h3>{props.dish.name}</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
+                    <RenderDish dish={props.dish}/>
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
-                </div>
+                    <RenderComments comments={props.comments}/>
+                </div>            
             </div>
         </div>
-    );
+    )
 }
 
+// Implementing "Submit Comment" & Applying Form Validation 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length < len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+export class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isModelOpen: false
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModelOpen: !this.state.isModelOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
+    }
+
+    render() {
+        return(
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                </Button>
+
+                <Modal isOpen={this.state.isModelOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label for="rating" md={12}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" name="rating" id="rating" defaultValue="1" className="form-control">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                <Control.text model=".author" id="author" name="author" 
+                                    placeholder="Your Name" 
+                                    className="form-control" 
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }} />
+                                <Errors className="text-danger" model=".author" show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="feedback" md={2}>Comment</Label>
+                                <Col md={12}>
+                                <Control.textarea model=".comment" id="comment" name="comment" 
+                                    resize="none"
+                                    rows="6" 
+                                    className="form-control" 
+                                    validators={{
+                                        required,
+                                    }} />
+                                <Errors className="text-danger" model=".comment" show="touched"
+                                    messages={{
+                                        required: 'Required'
+                                    }} />
+                                </Col>
+                            </Row>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>                            
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
 
 export default Dishdetail;
