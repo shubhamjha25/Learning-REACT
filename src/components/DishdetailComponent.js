d01ef728dbb02816ff0c88ceca6e3c98107f7140
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-12 23:47:01 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody,CardText, Button, Modal, ModalHeader, ModalBody
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderComments({comments, addComment, dishID}) {
     if (comments == null) {
@@ -38,7 +39,7 @@ function RenderDish({dish}) {
     if (dish != null) {
         return(
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
